31154cb540cf269eaae61754dc56fdaa76184a14
Samba Ndiaye <ext_ndiaye76@orange-sonatel.com>
2021-05-18 23:07:35 +0000

    Updated dashboard layout

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,10 +1,20 @@
-const modal = document.querySelector('.modal');
+var modal = document.querySelector('.modal');
 
 function openModal() {
+  event.preventDefault();
+  modal =
+    event.currentTarget.id === 'add-topping-modal'
+      ? document.getElementById('topping-modal')
+      : document.querySelector('.modal');
   modal.classList.add('active-modal');
 }
 
 function closeModal() {
-  // remove .active class
+  let target = $(event.currentTarget);
+  modal = target.parents('#topping-modal').length
+    ? document.getElementById('topping-modal')
+    : document.querySelector('.modal');
+
+  console.log(modal);
   modal.classList.remove('active-modal');
 }
